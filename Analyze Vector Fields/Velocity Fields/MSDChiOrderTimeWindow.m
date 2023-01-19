47da8805fab699886b6abac5f1dc16d42457b564
Herodot1 <59092897+Herodot1@users.noreply.github.com>
2023-01-19 16:50:42 +0100

    Update MSDChiOrderTimeWindow.m
    Small change on how the end of a track of a virtual particle is determined. Sometimes the old way led to inaccuracies due to unwanted continuiation of a track.

--- Analyze Vector Fields/Velocity Fields/MSDChiOrderTimeWindow.m
+++ Analyze Vector Fields/Velocity Fields/MSDChiOrderTimeWindow.m
@@ -35,8 +35,10 @@ for k = 1+(WSize-1)/2:Sampling:size(VelField,4)-1-((WSize-1)/2)
             % Get points were objects move out of image (<=0) or detection has
             % been abandoned (=NaN)
             ende = find(isnan(Trajectory) == 1,1);
-            ende2 = find(Trajectory(:,1) <= 0,1);
-            ende3 = find(Trajectory(:,2) <= 0,1);
+            %ende2 = find(Trajectory(:,1) <= 0,1);
+            %ende3 = find(Trajectory(:,2) <= 0,1);
+            ende2 = find(diff(Trajectory(:,1)) == 0,1);
+            ende3 = find(diff(Trajectory(:,2)) == 0,1);
             ende = min([ende;ende2;ende3;WSize]);
             % Calculate relative distance for Order Parameter and MSD:
             pos = size(TrackMat,2)*(i-1) + j;                     
