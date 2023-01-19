2ff4be675ccf1e5f5b4cb871604f8ac235158e8b
Herodot1 <59092897+Herodot1@users.noreply.github.com>
2023-01-19 16:41:22 +0100

    Files for actual calculation of Caged MSD

---
+++ Analyze Vector Fields/Velocity Fields/MSDCagedTemp.m
@@ -0,0 +1,76 @@
+function [MSDCagedTemp] = MSDCagedTemp(VelField,WSize,dt,im_size,POIx,POIy,SubPxResolution,Sampling,CenterSpeed)
+
+
+MSDCagedTemp = NaN(WSize,size(VelField,4),10);
+Trajectory = [];
+TrackMatTemp = cell(1,numel(1+(WSize-1)/2:Sampling:size(VelField,4)-1-((WSize-1)/2)));   
+
+count = 0;
+for k = 1+(WSize-1)/2:Sampling:size(VelField,4)-1-((WSize-1)/2)
+    count = count + 1;
+    idx = k - (WSize-1)/2;
+    %Percentage = (k-(WSize-1)/2)/(size(VelField,4)-1-(WSize-1)/2)
+    interval = k-(WSize-1)/2:k+(WSize-1)/2;    
+    [TrackMat] = GenTrackMat(VelField(:,:,:,interval),im_size,POIx,POIy,SubPxResolution,CenterSpeed);
+    %TrackMatTemp{count} = TrackMat;
+    TrackMat = TrackMat(2:end-1,2:end-1,:,:);          
+    % Nearest Neighbors of each non-boundary point:
+    TempVarXAll = squeeze(TrackMat(1:end,1:end,2,1));
+    TempVarYAll = squeeze(TrackMat(1:end,1:end,1,1));
+    XStartAll = [TempVarXAll(:),TempVarYAll(:)];    
+    % Allocate variables:
+    MSDCaged = NaN(WSize,size(TrackMat,1)*size(TrackMat,2),10);    
+    countNeighbors = 0;
+    for NumNeighborsKNN = [1:10].^2%[1,9,25,49,81,121]
+        countNeighbors = countNeighbors+1;
+        % size constraints for usage of points in tracking matrix:
+        Constraint = 1+ceil((sqrt(NumNeighborsKNN)-1)/2);
+        
+        % get constraint points of interest:
+        TempVarXConstraint = squeeze(TrackMat(Constraint:end-Constraint,Constraint:end-Constraint,2,1));
+        TempVarYConstraint = squeeze(TrackMat(Constraint:end-Constraint,Constraint:end-Constraint,1,1));
+        XStartConstraint = [TempVarXConstraint(:),TempVarYConstraint(:)];
+        % Nearest Neighbors of each point:
+        [IdxStart,DStart] = knnsearch(XStartAll,XStartAll,'K',NumNeighborsKNN);
+        % Identify points of interest:
+        IdxPOI = find(ismember(XStartAll,XStartConstraint,'rows') == 1);
+        % Get the associated coordinates in the tracking matrix:
+        [rowPOI,colPOI] = ind2sub(size(squeeze(TrackMat(:,:,1,1))), IdxPOI);
+        for j = 1:length(IdxPOI)
+            % get rows and columns of relevant data. denote that the point of
+            % interest is also included in the list (as it is one of its
+            % nearest neighbors ...):
+            [rowStart,colStart] = ind2sub(size(TempVarXAll), IdxStart(IdxPOI(j),1:end));
+            % get all coordinates of neighbors:
+            Coord = NaN(2,length(rowStart),size(TrackMat,4));
+            for k = 1:length(rowStart)
+                Coord(1,k,:) = squeeze(TrackMat(rowStart(k),colStart(k),1,:));
+                Coord(2,k,:) = squeeze(TrackMat(rowStart(k),colStart(k),2,:));
+            end
+            % Generate trajecteroy(x,y,t) of cage by substracting the mean
+            % position of the neighbors from the position of interest.
+            % denote the substraction of the point of interest before
+            % calculating the mean, as it is also included in the list of
+            % neighbors.
+            % the initial
+            
+            Trajectory(:,1) = squeeze(TrackMat(rowPOI(j),colPOI(j),1,:)) - (squeeze(sum(Coord(1,:,:),2)) - squeeze(TrackMat(rowPOI(j),colPOI(j),1,:)))./max([length(rowStart)-1,1]);
+            Trajectory(:,2) = squeeze(TrackMat(rowPOI(j),colPOI(j),2,:)) - (squeeze(sum(Coord(2,:,:),2)) - squeeze(TrackMat(rowPOI(j),colPOI(j),2,:)))./max([length(rowStart)-1,1]);
+            Trajectory(:,3) = [0:size(TrackMat,4)-1]*dt;
+            % Get points were objects move out of image (change ==0) or detection has
+            % been abandoned (=NaN)
+            ende = find(isnan(Trajectory) == 1,1);
+            ende2 = find(diff(TrackMat(rowPOI(j),colPOI(j),1,:)) == 0,1);
+            ende3 = find(diff(TrackMat(rowPOI(j),colPOI(j),2,:)) == 0,1);
+            %ende = min([ende;ende2;ende3;size(TrackMat,4)+1]);
+            ende = min([ende;ende2;ende3;size(TrackMat,4)+1]);
+            % Get MSD:
+            [temp,tau] = Kehl(Trajectory(1:ende-1,:));
+            %pos = size(TrackMat,2)*(i-1) + j;
+            %MSDCaged(1:ende-2,pos,countNeighbors) = temp;
+            MSDCaged(1:ende-2,j,countNeighbors) = temp;
+        end
+        MSDCaged(MSDCaged == 0) = NaN;
+    end
+    MSDCagedTemp(:,idx,:) = nanmean(MSDCaged,2);
+end
